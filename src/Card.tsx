6d84303e79e8c65cf80c4e1adbe06b5fe064a487
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-01-01 13:07:54 +0530

    Update package details and add social media sharing icons

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,8 @@
 import pokemonData from "./types.ts"
+import {
+  FaFacebookF,
+  FaTwitter,
+} from "react-icons/fa6"
 
 function TypeLabel(props: { type: string }) {
   console.log(props.type)
@@ -11,7 +15,7 @@ function TypeLabel(props: { type: string }) {
 
 function AbilitiesLabel(props: { ability: string }) {
   return (
-    <label className="px-3 py-1 mx-1 bg-gray-200 rounded-xl first-letter:uppercase">
+    <label className="px-3 py-1 mx-1 bg-gray-200 bg-opacity-50 rounded-xl font-semibold first-letter:uppercase">
       {props.ability}
     </label>
   )
@@ -19,7 +23,7 @@ function AbilitiesLabel(props: { ability: string }) {
 
 export default function Card({ Data }: { Data: pokemonData | undefined }) {
   return (
-    <div className="flex flex-col items-center justify-center w-screen md:w-96 pb-4 bg-black bg-opacity-10 rounded-none md:rounded-xl shadow-xl hover:shadow-2xl">
+    <div className="flex flex-col items-center justify-center w-screen md:w-96 pb-4 bg-black bg-opacity-10 rounded-none md:rounded-xl shadow-none md:shadow-xl md:hover:shadow-2xl my-0">
       {/* Image */}
       <img src={Data?.imageURL} alt={Data?.name} className="w-full border-b rounded-t-xl" loading="lazy" />
 
@@ -36,7 +40,7 @@ export default function Card({ Data }: { Data: pokemonData | undefined }) {
       </div>
 
       {/* Stats */}
-      <div className="flex flex-col items-center justify-center w-full h-1/2 my-2">
+      <div className="flex flex-col items-center justify-center w-full h-1/2 mt-2">
         <div className="flex flex-row items-center justify-center w-full h-1/2">
           <div className="flex flex-col items-center justify-center w-1/3 h-full">
             <h2 className="text-2xl text-center font-bold">Weight</h2>
@@ -53,11 +57,32 @@ export default function Card({ Data }: { Data: pokemonData | undefined }) {
         </div>
 
         {/* Abilities  */}
-        <div className="flex flex-row items-center justify-center w-full h-1/2">
+        <div className="flex flex-row items-center justify-center w-full h-1/2 mt-2">
           <div className="flex flex-col items-center justify-center w-full h-full">
             <h2 className="text-2xl font-bold">Abilities</h2>
             <div className="flex flex-row items-center justify-center w-full h-full">
               {Data?.abilities.map((ability, index) => <AbilitiesLabel ability={ability} key={index} />)}
+            </div>
+          </div>
+        </div>
+
+        {/* Share on Social Media */}
+        <div className="flex flex-row items-center justify-center w-full h-1/2 mt-2">
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            <h2 className="text-2xl font-bold">Share on Social Media</h2>
+            <div className="flex flex-row items-center justify-center w-full h-full">
+              <a href={`https://twitter.com/intent/tweet?text=I%20just%20got%20a%20${Data?.name}%20on%20PokeNode%20TS!%20Check%20it%20out%20here:%20https://pokenode-ts.vercel.app`} target="_blank" rel="noopener noreferrer" className="p-2 mx-1 bg-blue-500 rounded-xl font-semibold shadow-sm uppercase text-white">
+                <FaTwitter className="inline-flex mr-1" />
+                <p className="inline-flex">
+                  Twitter
+                </p>
+              </a>
+              <a href={`https://www.facebook.com/sharer/sharer.php?u=https://pokenode-ts.vercel.app`} target="_blank" rel="noopener noreferrer" className="p-2 mx-1 bg-blue-800 rounded-xl font-semibold shadow-sm uppercase text-white">
+                <FaFacebookF className="inline-flex mr-1" />
+                <p className="inline-flex">
+                  Facebook
+                </p>
+              </a>
             </div>
           </div>
         </div>
